c18c3fecea4205ed11933443d478d7a274c4c682
Laura Ramos <lauramilena.rbe@gmail.com>
2017-11-12 22:22:21 -0500

    fixed error2

--- src/main/java/edu/eci/arsw/collabhangman/cache/redis/GameStateRedisCache.java
+++ src/main/java/edu/eci/arsw/collabhangman/cache/redis/GameStateRedisCache.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
  *
  * @author laura
  */
-@Service
+/*@Service*/
 public class GameStateRedisCache {
     @Autowired
     private StringRedisTemplate template;
